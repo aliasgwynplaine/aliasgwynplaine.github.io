09553eaf5a501bbb7fb4965bb7a7c1bc36d2ccca
gwynplaine <mnleonr@uni.pe>
2023-08-10 15:08:26 -0500

    Solved the problem with the accents

--- assets/js/sorting.js
+++ assets/js/sorting.js
@@ -1,3 +1,5 @@
+goa = s => s.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+
 function filterTable(inputid, tableid) {
     var table = document.getElementById(tableid)
     var input = document.getElementById(inputid)
@@ -5,7 +7,7 @@ function filterTable(inputid, tableid) {
     var rows  = table.rows
 
     for (var i = 0; i < rows.length; i++) {
-        if (rows[i].id.indexOf(word) > -1)
+        if (goa(rows[i].id).indexOf(goa(word)) > -1)
             rows[i].style.display = ""
         else rows[i].style.display = "None"
     }
@@ -27,6 +29,6 @@ function quicksort(arr, cmpfux) {
 function sortTableRowsByIdAlpha(tableid) {
     var table  = document.getElementById(tableid)
     var rows   = table.rows
-    var sorted = quicksort(rows, (a, b) => a.id < b.id) // todo: handle accents
+    var sorted = quicksort(rows, (a, b) => goa(a.id) < goa(b.id)) 
     sorted.forEach(tr => table.appendChild(tr));
 }
