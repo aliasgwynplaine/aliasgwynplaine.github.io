f3399c6c9044c93d79639ef019a749652c9c1776
gwynplaine <mnleonr@uni.pe>
2023-08-11 16:05:25 -0500

    fixed some bug

--- assets/js/sorting.js
+++ assets/js/sorting.js
@@ -4,11 +4,12 @@ function filterTable(inputid, tableid) {
     var table   = document.getElementById(tableid)
     var input   = document.getElementById(inputid)
     var word    = input.value
-    var rows    = table.rows
+    var rows    = Array.from(table.rows)
     var nomatch = true
-    rows[rows.length - 1].style.display = "None"
+    rows.pop()
+    document.getElementById('no-match').style.display = "None"
 
-    for (var i = 0; i < rows.length - 1; i++) {
+    for (var i = 0; i < rows.length; i++) {
         if (goa(rows[i].id).indexOf(goa(word)) > -1) {
             rows[i].style.display = ""
             nomatch = false
@@ -16,7 +17,7 @@ function filterTable(inputid, tableid) {
         else rows[i].style.display = "None"
     }
 
-    if (nomatch) rows[rows.length - 1].style.display = ""
+    if (nomatch) document.getElementById('no-match').style.display = ""
 
 }
 
@@ -27,15 +28,17 @@ function quicksort(arr, cmpfux) {
     var l = []
     var r = []
 
-    for (var i = 1; i < arr.length - 1; i++)
+    for (var i = 1; i < arr.length; i++){
         cmpfux(arr[i], p) ? l.push(arr[i]) : r.push(arr[i])
-
+    }
     return quicksort(l, cmpfux).concat(p, quicksort(r, cmpfux))
 }
 
 function sortTableRowsByIdAlpha(tableid) {
     var table  = document.getElementById(tableid)
-    var rows   = table.rows
+    var rows   = Array.from(table.rows)
+    rows.pop()
     var sorted = quicksort(rows, (a, b) => goa(a.id) < goa(b.id)) 
+    
     sorted.forEach(tr => table.appendChild(tr));
 }
